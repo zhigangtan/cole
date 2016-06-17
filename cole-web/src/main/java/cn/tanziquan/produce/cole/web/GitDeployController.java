a11d5e8a6beddf7acfe4a782c131d404e278605c
谭志刚 <zhigang.tan@ele.me>
2016-06-17 21:34:30 +0800

    no message

--- cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
+++ cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/deploy")
 public class GitDeployController {
+    
 
     private static final Logger logger = LoggerFactory.getLogger(GitDeployController.class);
 
