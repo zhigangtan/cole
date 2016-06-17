d602238a455c99107bb0bd30889cdc7271e1e917
谭志刚 <zhigang.tan@ele.me>
2016-06-17 21:17:07 +0800

    no message

---
+++ cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
@@ -0,0 +1,24 @@
+package cn.tanziquan.produce.cole.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * GitDeployController
+ *
+ * @author zhigang.tan@ele.me
+ * @version 0.0.1
+ */
+@RestController
+@RequestMapping(value = "/deploy")
+public class GitDeployController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GitDeployController.class);
+
+    @RequestMapping(value = "/{appId}", method = RequestMethod.POST)
+    public void deploy(@PathVariable(value = "appId") String appId, @RequestBody String bodyJson) {
+        logger.info("appId:{},bodyJson:{}", appId, bodyJson);
+    }
+
+}
