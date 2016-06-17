5288a4b86e514788e1cbbe22ba70d55f7bda9ec9
谭志刚 <zhigang.tan@ele.me>
2016-06-17 21:42:34 +0800

    no message

--- cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
+++ cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/deploy")
 public class GitDeployController {
-    
+
 
     private static final Logger logger = LoggerFactory.getLogger(GitDeployController.class);
 
-    @RequestMapping(value = "/{appId}", method = RequestMethod.POST)
+    @RequestMapping(value = "/{appId:.+}", method = RequestMethod.POST)
     public void deploy(@PathVariable(value = "appId") String appId, @RequestBody String bodyJson) {
         logger.info("appId:{},bodyJson:{}", appId, bodyJson);
     }
