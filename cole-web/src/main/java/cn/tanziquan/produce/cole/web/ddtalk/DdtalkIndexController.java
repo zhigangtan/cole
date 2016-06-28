64b1fa50bc01d72a2c223eef42dd1bae10c8a3cf
谭志刚 <zhigang.tan@ele.me>
2016-06-28 17:21:57 +0800

    no message

--- cole-web/src/main/java/cn/tanziquan/produce/cole/web/ddtalk/DdtalkIndexController.java
+++ cole-web/src/main/java/cn/tanziquan/produce/cole/web/ddtalk/DdtalkIndexController.java
@@ -1,10 +1,7 @@
 package cn.tanziquan.produce.cole.web.ddtalk;
 
-import cn.tanziquan.produce.cole.configure.properties.DdtalkEnvProperties;
-import cn.tanziquan.produce.cole.ddtalkapp.service.IDdtalkAppService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +23,7 @@ public class DdtalkIndexController {
     private String index(@RequestParam(value = "corpid") String corpid) {
         logger.info("index corpid:{}", corpid);
 
-        return "ok";
+        return "ok index";
     }
 
 
@@ -34,6 +31,6 @@ public class DdtalkIndexController {
     private String admin(@RequestParam(value = "corpid") String corpid) {
         logger.info("admin corpid:{}", corpid);
 
-        return "ok";
+        return "ok admin ";
     }
 }
