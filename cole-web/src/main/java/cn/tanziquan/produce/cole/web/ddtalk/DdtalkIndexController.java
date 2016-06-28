30828956ed9b2732d513fe8b44d21620f801d1cb
谭志刚 <zhigang.tan@ele.me>
2016-06-28 21:10:00 +0800

    t

--- cole-web/src/main/java/cn/tanziquan/produce/cole/web/ddtalk/DdtalkIndexController.java
+++ cole-web/src/main/java/cn/tanziquan/produce/cole/web/ddtalk/DdtalkIndexController.java
@@ -1,5 +1,11 @@
 package cn.tanziquan.produce.cole.web.ddtalk;
 
+import com.dingtalk.open.client.ServiceFactory;
+import com.dingtalk.open.client.api.model.corp.SsoUserInfo;
+import com.dingtalk.open.client.api.service.corp.SsoService;
+import com.dingtalk.open.client.api.service.isv.IsvService;
+import com.dingtalk.open.client.common.SdkInitException;
+import com.dingtalk.open.client.common.ServiceNotExistException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DdtalkIndexController {
 
     private static final Logger logger = LoggerFactory.getLogger(DdtalkIndexController.class);
+    private SsoUserInfo userInfo;
 
 
     @RequestMapping(value = "/index")
@@ -30,6 +37,14 @@ public class DdtalkIndexController {
     @RequestMapping(value = "/admin")
     private String admin(@RequestParam(value = "code") String code) {
         logger.info("admin code:{}", code);
+        try {
+            ServiceFactory serviceFactory = ServiceFactory.getInstance();
+            SsoService ssoService = serviceFactory.getOpenService(SsoService.class);
+            SsoUserInfo userInfo=  ssoService.getSSOUserinfo("r9HWfoFsQT3Y9LoGFgpPqnFHXqjuGX73VTcwAeR7DbQGnKEAJid125nFgf2c-VwB",code);
+          logger.info("user:{},corpid", userInfo.getName());
+        } catch (Exception e) {
+            logger.error("get sso error", e);
+        }
 
         return "ok admin ";
     }
