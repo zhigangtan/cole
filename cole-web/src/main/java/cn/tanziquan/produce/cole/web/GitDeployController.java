cf54b90a35dcff285622dbeb34712fd7b5ecc84a
zhigang.tan <tanzhig@aliyun.com>
2016-06-30 16:59:44 +0800

    t

--- cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
+++ cole-web/src/main/java/cn/tanziquan/produce/cole/web/GitDeployController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class GitDeployController {
 
 
+
     private static final Logger logger = LoggerFactory.getLogger(GitDeployController.class);
 
     @Autowired
